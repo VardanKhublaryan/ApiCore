766440c3dd53084c19565fc91171ef198ddc1939
user <vardankhublaryan2@gmail.com>
2024-09-21 12:38:04 +0400

    change private info

--- src/main/java/apiCore/constants/RegisterConstants.java
+++ src/main/java/apiCore/constants/RegisterConstants.java
@@ -2,16 +2,16 @@ package apiCore.constants;
 
 public class RegisterConstants {
 
-    public final String CUSTOMER_ID = "9999999999";
+    public final String CUSTOMER_ID = "********";
     public final String CUSTOMER_TYPE = "ssn";
-    public final String NEW_CLIENT_NAME = "Helix TestB";
+    public final String NEW_CLIENT_NAME = "********";
     public final String SENDER_TIME = "240319121232";
 
-    public final String TRANSACTION_ID = "005240";
-    public final String BYBLOS_BANK_TOKEN = "BZIV364umbBVZn7UhX2JeYDTXmXbQm0U";
-    public final String TEST_BANK_TOKEN = "123456789123456789";
-    public final String ARMSOFT_TOKEN = "YJAIGSWIMBUYROUFLQRBBNEHEZTUFIOMNLXDLRQDNW";
-    public final String INCORRECT_OTP = "444587";
-    public final String INVALID_RELATIVE_NUMBER = "26296578-3b51-44bb-85c9-1b900588083i";
-    public final String WRONG_CUSTOMER_ID = "1111111111";
+    public final String TRANSACTION_ID = "********";
+    public final String BYBLOS_BANK_TOKEN = "********";
+    public final String TEST_BANK_TOKEN = "********";
+    public final String ARMSOFT_TOKEN = "********";
+    public final String INCORRECT_OTP = "********";
+    public final String INVALID_RELATIVE_NUMBER = "********-3b51-********-********-1b900588083i";
+    public final String WRONG_CUSTOMER_ID = "********";
 }
